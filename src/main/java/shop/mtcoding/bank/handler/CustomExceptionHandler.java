8845db8fe106fa9ef7819fa51063741f48c9599e
TobeInhyo <izp1012@tobeway.com>
2025-02-05 16:12:32 +0900

    1. 회원가입 컨트롤러 유효성 검사 2. 회원가입 컨트롤러 AOP 적용

--- src/main/java/shop/mtcoding/bank/handler/CustomExceptionHandler.java
+++ src/main/java/shop/mtcoding/bank/handler/CustomExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.xml.sax.helpers.DefaultHandler;
 import shop.mtcoding.bank.dto.ResponseDto;
 import shop.mtcoding.bank.handler.ex.CustomApiException;
+import shop.mtcoding.bank.handler.ex.CustomValidationException;
 
 @RestControllerAdvice
 public class CustomExceptionHandler extends DefaultHandler {
@@ -19,5 +20,11 @@ public class CustomExceptionHandler extends DefaultHandler {
     public ResponseEntity<?> apiException(CustomApiException e){
         logger.error(e.getMessage());
         return new ResponseEntity<>(new ResponseDto<>(-1, e.getMessage(), null), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(CustomValidationException.class)
+    public ResponseEntity<?>validationException(CustomValidationException e){
+        logger.error(e.getMessage());
+        return new ResponseEntity<>(new ResponseDto<>(-1, e.getMessage(), e.getErrorMap()), HttpStatus.BAD_REQUEST);
     }
 }
